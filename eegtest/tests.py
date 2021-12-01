cc94e6bcbda296752b658e96031431f6d2a09e83
nigoshaya <desm606@gmail.com>
2021-12-01 17:02:43 +0600

    added test

--- eegtest/tests.py
+++ eegtest/tests.py
@@ -26,3 +26,19 @@
 # #
 # # wsapp = websocket.WebSocketApp("wss://localhost:6868", on_message=on_message)
 # # wsapp.run_forever()
+import json
+
+import websocket
+import ssl
+
+websocket.enableTrace(True)
+ws = websocket.WebSocket(sslopt={"cert_reqs": ssl.CERT_NONE})
+ws.connect("wss://localhost:6868")
+get_cortex_info_request = {
+            "jsonrpc": "2.0",
+            "method": "getCortexInfo",
+            "id": 100
+        }
+ws.send(json.dumps(get_cortex_info_request))
+print(ws.recv())
+ws.close()

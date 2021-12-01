37971eb689f93ca6635ba573e1b59398b43dc46a
nigoshaya <desm606@gmail.com>
2021-12-01 19:54:01 +0600

    added ssl

--- eegtest/tests.py
+++ eegtest/tests.py
@@ -30,10 +30,14 @@
 import json
 import websocket
 import ssl
+import certifi
+
+# ssl_context = ssl.create_default_context()
+# ssl_context.load_verify_locations(certifi.where())
 
 websocket.enableTrace(True)
 ws = websocket.WebSocket(sslopt={"cert_reqs": ssl.CERT_NONE, "check_hostname": False})
-ws.connect("wss://127.0.0.1:6868", http_proxy_host="143.198.221.88", http_proxy_port="80", proxy_type="http")
+ws.connect("wss://127.0.0.1:6868", ssl=ssl.CERT_NONE, http_proxy_host="143.198.221.88", http_proxy_port="80", proxy_type="http")
 get_cortex_info_request = {
             "jsonrpc": "2.0",
             "method": "getCortexInfo",
